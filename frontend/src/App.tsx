3bc6937f90a0dd43247c38914b1df13d0da1e878
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-09-06 19:11:31 -0300

    apex-charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 
+import BarChart from 'components/barChart';
 import DataTable from 'components/datatable';
+import DunatChart from 'components/dunutChart';
 import Footer from 'components/footer';
 import { NavBar } from 'components/navbar';
 
@@ -8,8 +10,21 @@ function App() {
   return (
     <>
       <NavBar />
-      <div>
+      <div className="container">
+        <h1 className="text-primary py-3">DashBoards de vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DunatChart />
+          </div>
+        </div>
 
+
+        <h2 className="text-primary"> Todas as vendas</h2>
         <DataTable />
       </div>
       <Footer />
